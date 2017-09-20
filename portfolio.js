7012c2d4bc0870d0d9f988dcabceb8b95d7602c1
Thomas Pilnik <thomaspo@al.insper.edu.br>
2017-09-20 17:04:32 -0300

    atualizando

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,17 @@
 $(document).on('ready', function() {
- $(".slider").slick({
+ $(".center").slick({
   dots: false,
   infinite: true,
-  centerMode: false,
+  centerMode: true,
   slidesToShow: 6,
   slidesToScroll: 1
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  	var design = document.getElementById("design");
+  	design.addEventListener("click", function() {
+  	design.style.display= "inline-block";
 });
 });
