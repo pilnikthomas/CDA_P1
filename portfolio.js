742a97e4e504f01b8a284f1a7ced842ee702dbff
mariaeduardabicalho <mariaeduardabicalho@gmail.com>
2017-09-21 17:14:17 -0300

    quase

--- portfolio.js
+++ portfolio.js
@@ -8,9 +8,45 @@ $(document).on('ready', function() {
 });
 });
 
-function myFunction() {
+function fdesign() {
 	var x = document.getElementById("ds");
-    if (x.style.visibility === 'none') {
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function fcomu() {
+  var x = document.getElementById("com");
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function fprog() {
+  var x = document.getElementById("prog");
+    if (x.style.display === 'none') {
+        x.style.display = 'inline';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function fel() {
+  var x = document.getElementById("el");
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function fmod() {
+  var x = document.getElementById("mod");
+    if (x.style.display === 'none') {
         x.style.display = 'block';
     } else {
         x.style.display = 'none';
